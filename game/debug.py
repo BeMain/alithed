67b3c550c9d609a66f0ea175ca9f1d6362d87965
BeMain <bemain.dev@gmail.com>
2022-02-02 19:24:52 +0100

    Format debug.py

--- game/debug.py
+++ game/debug.py
@@ -2,12 +2,15 @@ import functools
 import time
 
 CLEAR_WORLD_ON_STARTUP = False
-ENABLED_LOGGING_PRIORITY = 2    # 3: Unimportant information
-                                # 2: Notes
-                                # 1: Errors
-                                # 0: Temporary logging 
 
-def log(data, priority = 0):
+# 3: Unimportant information
+# 2: Notes
+# 1: Errors
+# 0: Temporary logging
+ENABLED_LOGGING_PRIORITY = 2
+
+
+def log(data, priority=0):
     if priority <= ENABLED_LOGGING_PRIORITY:
         print(data)
 
@@ -20,5 +23,5 @@ def timeit(func):
         res = func(*args, **kwargs)
         log(f"Executing {func.__name__} took {time.time()-start}", priority=0)
         return res
-    
-    return wrapper 
+
+    return wrapper
