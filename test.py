158557c4199dd6fc36493dd4989334c482c582ea
BeMain <bemain.dev@gmail.com>
2022-02-04 18:41:38 +0100

    Perform some speed tests on current positions

--- test.py
+++ test.py
@@ -1,11 +1,29 @@
 import numpy as np
 
 from game import debug
-from game.positions import Tilepos
+from game.positions import *
+
+# === BENCHMARKS BEFORE ===
+# Creating Screenpos: 8.58306884765625e-06
+# Creating Screenpos + Worldpos: 1.3589859008789062e-05
+# + converting Screenpos -> Worldpos: 3.719329833984375e-05
+# + converting Worldpos -> tilepos (test1): 6.437301635742188e-05
+# Creating Pos3: 8.106231689453125e-06
+# + Creating Pos3 from Pos3 (test2): 1.1205673217773438e-05
 
 
+@debug.timeit
+def test1():
+    screenpos = Screenpos(1, 2)
+    playerpos = Worldpos(0, 0)
+    worldpos = screenpos.to_worldpos(playerpos)
+    tilepos = worldpos.to_tilepos()
 
-arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
 
-for x in np.nditer(arr):
-  print(x)
+@debug.timeit
+def test2():
+    posa = Pos3(1, 2)
+    posb = Pos3.from_pos3(posa)
+
+
+test()
